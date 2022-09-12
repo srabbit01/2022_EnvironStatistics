eb6fb2999a05bf75a192bea5ba2f12308884e8b9
srabbit01 <srabbit01@naver.com>
2022-09-13 07:26:35 +0900

    Generated

---
+++ Textbook1/HW3_chap04.py
@@ -0,0 +1,79 @@
+# HW3
+
+'''
+4-2. The Minitab output for a random sample of data is
+shown below. Some of the quantities are missing. Compute the
+values of the missing quantities.
+- Var: X
+- N: 16
+- Mean: ?
+- SE Mean:0.159
+- StDev: ?
+- Sum: 399.851
+'''
+
+'''
+4-5. Suppose we have a random sample of size 2n from a
+population denoted by X, and E(X)=u and V(X)=(시그마)^2.
+X1bar = 1/2n씨그마(2n/i=1)Xi and X2bar = 1/n씨그마(n/i=1)Xi 
+be two estimators of u. Which is the better estimator of u?
+Explain your choice.
+'''
+
+'''
+4-7. Suppose that 세타1 and 세타2 are unbiased estimators of the
+parameter 세타. We know that V(세타1^)=2 and V(세타2^)=4 Which
+estimator is better, and in what sense is it better?
+'''
+
+'''
+4-9. Calculate the relative efficiency of the two estimators in
+Exercise 4-7.
+'''
+
+'''
+4-10. Suppose that 세타1^ and 세타2^ are estimators of the parameter
+세타. We know that E(세타1^)=세타, E(세타2^)=세타/2, V(세타1^)=10,
+V(세타2^)=4. Which estimator is “better”? In what sense is it
+better?
+'''
+
+'''
+4-12. Let three random samples of sizes n1=20, n2=10,
+and n3=8 be taken from a population with mean u and variance
+세타^2. Let (S^2)1, (S^2)2, and (S^2)3 be the sample variances. Show that
+S^2=(20*(S^2)1+10*(S^2)2+8*(S^2)3)/38 is an unbiased estimator of 세타^2.
+'''
+
+'''
+4-15. A textile fiber manufacturer is investigating a new
+drapery yarn, which has a standard deviation of 0.3 kg. The
+company wishes to test the hypothesis H0: u=14 against
+H1: u<14, using a random sample of five specimens.
+(a) What is the P-value if the sample average is Xbar=13.7kg?
+(b) Find 베타 for the case where the true mean elongation force
+is 13.5 kg and we assume that 알파=0.05.
+(c) What is the power of the test from part (b)?
+4-16. Repeat Exercise 4-15 using a sample size of n=16
+and the same critical region.
+'''
+
+'''
+4-17. In Exercise 4-15 with n=5:
+(a) Find the boundary of the critical region if the type I error
+probability is specified to be 알파=0.01.
+(b) Find 베타 for the case when the true mean elongation force is 13.5 kg.
+(c) What is the power of the test?
+'''
+
+'''
+4-19. The heat evolved in calories per gram of a cement
+mixture is approximately normally distributed. The mean is
+thought to be 100 and the standard deviation is 2. We wish to
+test H0: u=100 versus H1: u not= 100 with a sample of n=9 specimens.
+(a) If the rejection region is defined as Xbar<98.5 or Xbar>101.5,
+find the type I error probability 알파.
+(b) Find 베타 for the case where the true mean heat evolved is 103.
+(c) Find 베타  for the case where the true mean heat evolved is
+105. This value of 베타 is smaller than the one found in part (b). Why?
+'''
