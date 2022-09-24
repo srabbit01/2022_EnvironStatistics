1ea5207fea39e8f9de073b2da67ff8983feea165
srabbit01 <srabbit01@naver.com>
2022-09-25 08:29:23 +0900

    Generated

---
+++ Textbook1/HW5_chap06.py
@@ -0,0 +1,148 @@
+# HW5
+
+'''
+6-1. Establishing the properties of materials is an important
+problem in identifying a suitable substitute for biodegradable
+materials in the fast-food packaging industry. Consider the
+following data on product density (g/cm3) and thermal conductivity
+K-factor (W/mK) published in Materials Research
+and Innovation (1999, pp. 2–8).
+     Thermal Product
+Conductivity Density
+           y       x
+      0.0480  0.1750
+      0.0525  0.2200
+      0.0540  0.2250
+      0.0535  0.2260
+      0.0570  0.2500
+      0.0610  0.2765
+'''
+
+'''
+6-3. Regression methods were used to analyze the data from
+a study investigating the relationship between roadway surface
+temperature (x) and pavement deflection ( y). The data follow.
+Temperature Deflection Temperature Deflection
+          x          y           x          y
+       70.0      0.621        72.7      0.637
+       77.0      0.657        67.8      0.627
+       72.1      0.640        76.6      0.652
+       72.8      0.623        73.4      0.630
+       78.3      0.661        70.5      0.627
+       74.5      0.641        72.1      0.631
+       74.0      0.637        71.2      0.641
+       72.4      0.630        73.0      0.631
+       75.2      0.644        72.7      0.634
+       76.0      0.639        71.4      0.638
+'''
+
+'''
+6-4. Turbidity is a measure of the cloudiness of the water and
+is used to indicate water quality levels. Higher turbidity levels
+are usually associated with higher levels of disease- causing
+microbes like viruses, bacteria, and parasites. The turbidity
+units of measure are reported as formazin suspension units, or
+FAUs. Data were collected on the Rio Grande River during the
+late spring and summer months in order to study the relationship
+between temperature and turbidity. The data follow.
+Temperature Turbidity Temperature Turbidity
+          x         y           x         y
+       22.9       125        26.1       100
+       24.0       118        26.9       105
+       22.9       103        22.8        55
+       23.0       105        27.0       267
+       20.5        26        26.1       286
+       26.2        90        26.2       235
+       25.8        99        26.6       265
+'''
+
+'''
+6-5. An article in Concrete Research (“Near Surface
+Characteristics of Concrete: Intrinsic Permeability,” Vol. 41,
+1989) presented data on compressive strength x and intrinsic
+permeability y of various concrete mixes and cures. The following
+data are consistent with those reported.
+Strength Permeability Strength Permeability
+       x            y        x            y
+     3.1         33.0      2.4         35.7
+     4.5         31.0      3.5         31.9
+     3.4         34.9      1.3         37.3
+     2.5         35.6      3.0         33.8
+     2.2         36.1      3.3         32.8
+     1.2         39.0      3.2         31.6
+     5.3         30.1      1.8         37.7
+     4.8         31.2
+'''
+
+'''
+6-7. Consider the data and simple linear regression in
+Exercise 6-1.
+(a) Find the mean thermal conductivity given that the product
+density is 0.2350.
+(b) Compute a 95% CI on this mean response.
+(c) Compute a 95% PI on a future observation when product
+density is equal to 0.2350.
+(d) What do you notice about the relative size of these two
+intervals? Which is wider and why?
+'''
+
+'''
+6-2 The number of pounds of steam used per month by a
+chemical plant is thought to be related to the average ambient
+temperature (in F도) for that month. The past year’s usage and
+temperature are shown in the following table.
+          Usage/        Usage/
+    Temp. 1000    Temp. 1000
+Month x  y     Month x  y
+Jan. 21 185.79 July 68 621.55
+Feb. 24 214.47 Aug. 74 675.06
+Mar. 32 288.03 Sept. 62 562.03
+Apr. 47 424.84 Oct. 50 452.93
+May 50 454.58 Nov. 41 369.95
+June 59 539.03 Dec. 30 273.98
+
+6-8. Consider the data and simple linear regression model in
+Exercise 6-2.
+(a) Find the mean pounds of steam given that the ambient
+temperature is 52 degrees.
+(b) Compute a 99% CI on this mean response.
+(c) Compute a 99% PI on a future observation when the
+ambient temperature is equal to 52 degrees.
+(d) What do you notice about the relative size of these two
+intervals? Which is wider and why?
+'''
+
+'''
+6-9. Consider the data and simple linear regression in
+Exercise 6-3.
+(a) Find the mean deflection given that the temperature is
+74.0 degrees.
+(b) Compute a 95% CI on this mean response.
+(c) Compute a 95% PI on a future observation when temperature
+is equal to 74.0 degrees.
+(d) What do you notice about the relative size of these two
+intervals? Which is wider and why?
+'''
+
+'''
+6-10. Consider the data and simple linear regression in
+Exercise 6-4.
+(a) Find the mean turbidity given that the temperature is
+25.0 degrees.
+(b) Compute a 95% CI on this mean response.
+(c) Compute a 95% PI on a future observation when temperature
+is equal to 25.0 degrees.
+(d) What do you notice about the relative size of these two
+intervals? Which is wider and why?
+'''
+
+'''
+6-11. Consider the data and simple linear regression model
+in Exercise 6-5.
+(a) Find the mean permeability given that the strength is 2.1.
+(b) Compute a 99% CI on this mean response.
+(c) Compute a 99% PI on a future observation when the
+strength is equal to 2.1.
+(d) What do you notice about the relative size of these two
+intervals? Which is wider and why?
+'''
